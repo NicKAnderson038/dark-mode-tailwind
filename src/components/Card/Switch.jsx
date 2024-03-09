8ecf7ba0d2a7e94b9b815e36fcc7bb2d2bfb6829
NicKAnderson038 <nick7anderson@protonmail.com>
2024-03-08 21:01:13 -0800

    clean up

--- src/components/Card/Switch.jsx
+++ src/components/Card/Switch.jsx
@@ -1,5 +1,11 @@
+/* eslint-disable react/prop-types */
 import './switch.css'
 
+/**
+ * @param {Object} param
+ * @param {Boolean} param.toggle
+ * @param {Function} param.setTheme
+ */
 export default function Switch({ toggle, setTheme }) {
     return (
         <label>
